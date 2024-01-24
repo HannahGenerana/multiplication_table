7114ba89c71c3803074e6d2ccf20d5ca26efff29
HannahGenerana <hannahgenerana@gmail.com>
2024-01-24 20:40:30 +0800

    pseudocode

---
+++ multiplication_table.py
@@ -0,0 +1,11 @@
+# # Print the multiplication table from 1 to 10
+
+# pseudocode
+
+# create an outer loop of 10 to print 10 rows
+
+# create a nested loop of 10
+
+# find the product to show the multiplication table
+
+# print in another line
